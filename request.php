fa00e3a9f50a015220768d8332d449dc61b9e485
RV2989 <rutujaverekar4@gmail.com>
2023-11-23 19:32:01 +0530

    update

--- request.php
+++ request.php
@@ -1,12 +1,19 @@
 <?php
-
 $curl = curl_init();
-
-$urls=['Integrated Tax'=>'https://taxinformation.cbic.gov.in/api/cbic-notification-msts/fetchNotificationByYearAndCategory?year=2023&page=0&size=99&taxId=1000001&category=Integrated%20Tax', 'Central Tax'=>'https://taxinformation.cbic.gov.in/api/cbic-notification-msts/fetchNotificationByYearAndCategory?year=2023&page=0&size=200&taxId=1000001&category=Central%20Tax'];
-
+$base_url = "https://taxinformation.cbic.gov.in/api/cbic-notification-msts/";
+$year=2017;
+$current_year = date("Y");
+$count=$current_year-2017+1;
+$counter=1;
+$jsonData = [];
+while ($counter<$count){
+$urls=[$base_url."fetchNotificationByYearAndCategory?page=0&size=200&taxId=1000001&category=Integrated%20Tax"
+];
   $results=[];
-  foreach($urls as $name => $url){
-curl_setopt_array($curl, [CURLOPT_URL=>$url,  CURLOPT_RETURNTRANSFER => true,
+$counter1=count($urls)-1;
+for($i=0; $i<=$counter; $i++ ){
+  $fullUrl = $base_url . $urls[$i] . "&year=$year";
+curl_setopt_array($curl, [CURLOPT_URL=>$fullUrl,  CURLOPT_RETURNTRANSFER => true,
 CURLOPT_ENCODING => '',
 CURLOPT_MAXREDIRS => 10,
 CURLOPT_TIMEOUT => 0,
@@ -35,14 +42,18 @@ $response = curl_exec($curl);
 //echo $response;
 if($response !== false){
   $json=json_decode($response,true);
-}
+
 if($json !== null){
-  $result[$name]=$json;
+  $result[$year]=$json;
 }
 //echo '<pre>';
 //print_r($json);
 //echo '</pre>';
+$year++;
+$counter++;
   }
+}
+}
 $base_url='http://vinfur.in';
 header("Content-Type: text/xml;charset=iso-8859-1");
 echo "<?xml version='1.0' encoding='UTF-8' ?>" . PHP_EOL;
@@ -53,27 +64,28 @@ echo "<link>".$base_url."index.php</link>".PHP_EOL;
 echo "<description>Central Tax Notification</description>".PHP_EOL;
 echo "<language>en-us</language>".PHP_EOL;
 
-curl_close($curl);
+
 $counter=count($json)-1;
 
 for($i=0;$i<=$counter;$i++){
-    echo "<item>".PHP_EOL;
-    echo "<title>".clean($json[$i]['notificationName'])."</title>".PHP_EOL;
-   
-    echo "<link>https://old.cbic.gov.in/resources//htdocs-cbec/gst/".$json[$i]['docFileName']."</link>".PHP_EOL;
-    echo "<guid>https://old.cbic.gov.in/resources//htdocs-cbec/gst/".$json[$i]['docFileName']."</guid>".PHP_EOL;
-    //echo "<pubDate>".$json[$i]['notificationDt']."</pubDate>".PHP_EOL;
-    //echo "<description><![CDATA[".substr($row["post_description"], 0, 300) ."]]></description>".PHP_EOL;
-    /*  echo "<dc:creator>".$row["author"]."</dc:creator>".PHP_EOL;
-    
-    echo "<enclosure url='images/".$row["post_image"]."' length='".$image_size."' type='".$image_mime."' />".PHP_EOL;
-    echo "<category>PHP tutorial</category>".PHP_EOL;   */
-    echo "</item>".PHP_EOL;
+  echo "<item>".PHP_EOL;
+  echo "<title>".clean($json[$i]['notificationName'])."</title>".PHP_EOL;
+ 
+  echo "<link>https://old.cbic.gov.in/resources//htdocs-cbec/gst/".$json[$i]['docFileName']."</link>".PHP_EOL;
+  echo "<guid>https://old.cbic.gov.in/resources//htdocs-cbec/gst/".$json[$i]['docFileName']."</guid>".PHP_EOL;
+  //echo "<pubDate>".$json[$i]['notificationDt']."</pubDate>".PHP_EOL;
+  //echo "<description><![CDATA[".substr($row["post_description"], 0, 300) ."]]></description>".PHP_EOL;
+  /*  echo "<dc:creator>".$row["author"]."</dc:creator>".PHP_EOL;
+  
+  echo "<enclosure url='images/".$row["post_image"]."' length='".$image_size."' type='".$image_mime."' />".PHP_EOL;
+  echo "<category>PHP tutorial</category>".PHP_EOL;   */
+  echo "</item>".PHP_EOL;
 
 }
-echo '</channel>'.PHP_EOL;
-echo '</rss>'.PHP_EOL;
-
+  echo '</channel>'.PHP_EOL;
+  echo '</rss>'.PHP_EOL;
+  
+  curl_close($curl);
 function clean($string) {
     $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
     $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
